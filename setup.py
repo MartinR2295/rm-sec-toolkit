a806ab02f10ec5aac8d9704da12ecc9b6ceeb25e
Martin Rader <mrader@Martins-MacBook-Pro-2.local>
2021-05-09 10:05:48 +0300

    bugfix

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="rm-sec-toolkit",
-    version=UpdateHelper.get_current_version_number,
+    version=UpdateHelper.get_current_version_number(),
     description="module based security toolkit framework for python",
     long_description=README,
     long_description_content_type="text/markdown",

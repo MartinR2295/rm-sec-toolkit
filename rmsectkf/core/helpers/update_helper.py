d6ca6e89b326bd3667bf4757727d7cb1aec26221
Martin Rader <mrader@Martins-MacBook-Pro-2.local>
2021-05-09 12:06:04 +0300

    docker test update

--- rmsectkf/core/helpers/update_helper.py
+++ rmsectkf/core/helpers/update_helper.py
@@ -20,7 +20,7 @@ class UpdateHelper(object):
     '''
     @staticmethod
     def get_current_version_number():
-        version_number = "{{number: 0.1.15}}"
+        version_number = "{{number: 0.1.16}}"
         return version_number.replace("{{number: ", "").replace("}}", "")
 
 
